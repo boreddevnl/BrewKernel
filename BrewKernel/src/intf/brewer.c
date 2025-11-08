b11ca11d77e15105a1aae8142ea7b3b5ff776222
Chris <chris@boreddev.nl>
2025-11-08 19:51:36 +0100

    License headers

--- BrewKernel/src/intf/brewer.c
+++ BrewKernel/src/intf/brewer.c
@@ -1,7 +1,24 @@
 /* brewer.c
  * Implementation of the simple .brew interpreter.
+ * only function is brew_str, other functions will be implemented later i'm only testing out the possibility of a simple language inside the kernel
  */
-
+/*
+ * Brew Kernel
+ * Copyright (C) 2024-2025 boreddevhq
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "APPS/brewer.h"
 #include "print.h"
 #include "file.h"
